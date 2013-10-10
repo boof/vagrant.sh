c8cd71aff8b8d3b7abd87205b457697db47eb7a3
Florian Aßmann <flazy@fork.de>
2013-10-10 12:36:21 +0200

    The ruby versioning scheme works different in Debian/Ubuntu. Fixed update-alternatives with ruby module.

--- ruby.sh
+++ ruby.sh
@@ -5,11 +5,17 @@ provision apt
 # installs Ruby (defined in /vagrant/.ruby-version or given as parameter)
 # and modifies PATH
 function set-ruby () {
-    version=`cat /vagrant/.ruby-version 2>/dev/null`
-    version=${1:-$version}
+	version=`cat /vagrant/.ruby-version 2>/dev/null`
+	version=${1:-$version}
 
-    apt-install ruby$version
-    update-alternatives --set ruby /usr/bin/ruby$version >/dev/null
+	if [ "v$version" \< "v1.9" ]; then
+		version=1.8
+	else
+		version=1.9.1
+	fi
+
+	apt-install ruby$version
+	update-alternatives --set ruby /usr/bin/ruby$version >/dev/null
 }
 
 provision ruby-essential
