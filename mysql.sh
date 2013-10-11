6a6479569684f77bbf7b1580352a9bb5d5488f92
Florian Aßmann <flazy@fork.de>
2013-10-11 17:36:06 +0200

    Introduced module interface database.
    - added create-db
    - added create-db-user
    - pgsql now allows local connections w/o password
    - pgsql now provides database
    - mysql now provides database

--- mysql.sh
+++ mysql.sh
@@ -1,15 +1,31 @@
 #!/usr/bin/env bash
 
 provision apt
+provides database
+
+function create-db-user () {
+    local username=${1}
+    local password=${2:-$username}
+    mysql "CREATE USER '${username}'@'localhost' IDENTIFIED BY '${password}';"
+}
+function create-db () {
+    local db=${1}
+    local user=${2:-vagrant}
+    mysql \
+<<SQL
+    CREATE DATABASE IF NOT EXISTS ${db} CHARACTER SET utf8;
+    GRANT ALL PRIVILEGES ON ${db}.* TO '${user}'@'localhost' IDENTIFIED BY '${user}';
+SQL
+}
 
 # setup mysql-server to work well with vagrant
 has mysql-server-5.5 || {
     echo "Setting up MySQL server..."
 
     echo 'mysql-server-5.5 mysql-server/root_password password root' \
-        | debconf-set-selections
+    | debconf-set-selections
     echo 'mysql-server-5.5 mysql-server/root_password_again password root' \
-        | debconf-set-selections
+    | debconf-set-selections
     apt-install mysql-server-5.5
 
     echo '[client]'       > ~/.my.cnf
@@ -17,12 +33,8 @@ has mysql-server-5.5 || {
     echo 'password=root' >> ~/.my.cnf
     chmod 600 ~/.my.cnf
 
-    mysql \
-<<SQL
-    CREATE DATABASE IF NOT EXISTS vagrant CHARACTER SET utf8;
-    CREATE USER 'vagrant'@'localhost' IDENTIFIED BY 'vagrant';
-    GRANT ALL PRIVILEGES ON vagrant.* TO 'vagrant'@'localhost' IDENTIFIED BY 'vagrant';
-SQL
+    create-db vagrant
+    create-db-user vagrant
 
     echo '[client]'          > /home/vagrant/.my.cnf
     echo 'user=vagrant'     >> /home/vagrant/.my.cnf
--- pgsql.sh
+++ pgsql.sh
@@ -1,12 +1,30 @@
 #!/usr/bin/env bash
 
+provides database
 provision apt
+
+function create-db-user () {
+	local user=${1}
+	sudo -u postgres createuser --superuser $user
+}
+
+function create-db () {
+	local db=${1}
+	local user=${2:-vagrant}
+	sudo -u postgres createdb --owner=$user $db
+}
 
 # setup postgresql to work well with vagrant
 has postgresql-9.1 || {
-    echo "Setting up PostgreSQL server..."
-    apt-install postgresql-9.1
+	echo "Setting up PostgreSQL server..."
+	apt-install postgresql-9.1
 
-    sudo -u postgres createuser --superuser vagrant
-    sudo -u postgres createdb --owner=vagrant vagrant
+	create-db-user vagrant
+	create-db vagrant
+
+	local search='local   all             all                                     peer'
+	local replace='local   all             all                                     trust'
+	local search_and_replace="s/${ search }/${ replace }/"
+
+	sed -i $search_and_replace /etc/postgresql/9.1/main/pg_hba.conf
 }
